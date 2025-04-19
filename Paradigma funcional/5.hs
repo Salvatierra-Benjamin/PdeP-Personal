f1f36b4c72e3df6a50288c6209cc5eb943faa9fc
Benjamin Salvatierra <bsalvatierratola@frba.utn.edu.ar>
2025-04-18 23:20:55 -0300

    ejs 1-4

---
+++ Paradigma funcional/5.hs
@@ -0,0 +1,10 @@
+{-
+Definir la función esBisiesto/1, indica si un año es bisiesto. (Un año es bisiesto si es divisible por 400 o es
+divisible por 4 pero no es divisible por 100) Nota: Resolverlo reutilizando la función esMultiploDe/2
+-}
+
+-- seguir con esto
+
+esMultiploDe num1 num2 = num1 `mod` num2 == 0
+
+esBisiesto anio = esMultiploDe anio 400
