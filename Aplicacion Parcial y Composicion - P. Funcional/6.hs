08d69463bc1b786bc832d918d43199685d45fa06
Benjamin Salvatierra <bsalvatierratola@frba.utn.edu.ar>
2025-04-22 19:40:49 -0300

    farmeando commits

---
+++ Aplicacion Parcial y Composicion - P. Funcional/6.hs
@@ -0,0 +1,18 @@
+{-
+Resolver la función del ejercicio 2 de la guía anterior esMultiploDe/2,
+utilizando aplicación parcial y composición.
+-}
+
+{-
+Definir la función esMultiploDe/2, que devuelve True si el segundo es múltiplo del primero, p.ej.
+Main> esMultiploDe 3 12
+True
+-}
+
+division num = (/ 2)
+
+esMultiploDe num num2 = num2 `mod` num == 0
+
+hola x = (== 0) x
+
+-- (==0) es una funcion, y recibe un parametro. devuelve un booleano.
