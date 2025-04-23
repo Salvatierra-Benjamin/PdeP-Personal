9b6adf7726441e338d5bdac5cf77e3bc7711959e
Benjamin Salvatierra <bsalvatierratola@frba.utn.edu.ar>
2025-04-23 15:56:03 -0300

    prueba 123

---
+++ Aplicacion Parcial y Composicion - P. Funcional/8.hs
@@ -0,0 +1,10 @@
+{-
+Resolver la función inversaRaizCuadrada/1, que da un número n devolver
+la inversa su raíz cuadrada.
+Main> inversaRaizCuadrada 4
+0.5
+Nota: Resolverlo utilizando la función inversa Ej. 2.3, sqrt y composición.
+
+-}
+
+inversaRaizCuadrada num = num
---
+++ Aplicacion Parcial y Composicion - P. Funcional/prueba.hs
@@ -0,0 +1,5 @@
+sumar :: (Num a) => a -> a -> a
+sumar x y = x + y
+
+sumar5 :: Integer -> Integer
+sumar5 = sumar 5
