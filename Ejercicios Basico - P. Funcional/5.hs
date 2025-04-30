5e76f9007aca6f8d99dbe6162561819ca1e00d8b
Benjamin Salvatierra <bsalvatierratola@frba.utn.edu.ar>
2025-04-30 20:50:16 -0300

    Correción ejercicio 5

--- Ejercicios Basico - P. Funcional/5.hs
+++ Ejercicios Basico - P. Funcional/5.hs
@@ -5,6 +5,6 @@ divisible por 4 pero no es divisible por 100) Nota: Resolverlo reutilizando la f
 
 -- seguir con esto
 
-esMultiploDe num1 num2 = num1 `mod` num2 == 0
+esMultiploDe num1 num2 = mod num1 num2 == 0
 
-esBisiesto anio = esMultiploDe anio 400 || esMultiploDe anio 100
+esBisiesto anio = esMultiploDe anio 400 || (not (esMultiploDe anio 100) && esMultiploDe anio 4)
