785ed60bfd4e78041e779d432d5a60ba59e84585
Salvatierra-Benjamin <201190726+Salvatierra-Benjamin@users.noreply.github.com>
2026-02-20 15:05:37 -0300

    haskellPark actualizado

--- Haskell/jueves-noche/Practica-Haskellpark/haskellpark.hs
+++ Haskell/jueves-noche/Practica-Haskellpark/haskellpark.hs
@@ -5,6 +5,7 @@ data Atraccion = Atraccion
     opiniones :: [String],
     enMantenimiento :: Bool,
     ordenReparacion :: [Reparacion]
+    -- ordenReparacion :: [[20, ajustartornillo], [15, engrasar]]
   }
 
 data Reparacion = Reparacion
@@ -21,7 +22,7 @@ type Parque = [Atraccion]
 queTanBuenaEs :: Atraccion -> Int
 queTanBuenaEs atraccion
   | duracionProlongada atraccion = 100
-  | cantidadOrdenesDeReperacion atraccion < 3 = (((+) (cantidadOpiniones atraccion)) . (* 10) . puntajePorLargoDeNombre) atraccion
+  | cantidadOrdenesDeReperacion atraccion < 3 = (((+) (2 * cantidadOpiniones atraccion)) . (* 10) . puntajePorLargoDeNombre) atraccion
   --   | cantidadOrdenesDeReperacion atraccion < 3 = (+) (cantidadOpiniones atraccion) ((*) 10 (puntajePorLargoDeNombre atraccion))
   | otherwise = ((* 10) . alturaMinima) atraccion
 
@@ -32,7 +33,7 @@ puntajePorLargoDeNombre :: Atraccion -> Int
 puntajePorLargoDeNombre = length . nombre
 
 duracionProlongada :: Atraccion -> Bool
-duracionProlongada = (>) 10 . duracionAtraccion
+duracionProlongada = (<) 10 . duracionAtraccion
 
 cantidadOrdenesDeReperacion :: Atraccion -> Int
 cantidadOrdenesDeReperacion = length . ordenReparacion
@@ -58,6 +59,7 @@ modificadorOpiniones modificador atraccion = atraccion {opiniones = (modificador
 
 -- ?MODIFICADORES */
 
+-- Este efectosDeReperacion se utilizara solo al aplicar una Reperacion
 efectosDeReperacion :: Atraccion -> Atraccion
 efectosDeReperacion = tieneReparacionesPendientes . eliminarUltimaReperacion
 
@@ -67,8 +69,9 @@ eliminarUltimaReperacion = modificadorOrdenReparacion (sacarUltimoElemento)
 sacarUltimoElemento :: [a] -> [a]
 sacarUltimoElemento lista = take ((length lista) - 1) lista
 
--- Drop te saca una N cantidad de la lista
--- Take te saca la posicion N de la lista
+-- Take te devuelve los primeros N elementos
+-- ghci> take 3 [10,20,30,40,50]
+-- [10,20,30]
 
 tieneReparacionesPendientes :: Atraccion -> Atraccion
 tieneReparacionesPendientes atraccion
@@ -76,13 +79,19 @@ tieneReparacionesPendientes atraccion
   | otherwise = modificadorMantenimiento False atraccion
 
 ajusteDeTornilleria :: Int -> Trabajo
-ajusteDeTornilleria cantTornillos atraccion = modificadorDuracionAtraccion ((+) (maximaDuracionAtraccion atraccion cantTornillos)) atraccion
+-- ajusteDeTornilleria cantTornillos atraccion = modificadorDuracionAtraccion ((+) (maximaDuracionAtraccion atraccion cantTornillos)) atraccion
+ajusteDeTornilleria cantTornillos atraccion = setearDuracion (maximaDuracionAtraccion atraccion cantTornillos) atraccion
 
 maximaDuracionAtraccion :: Atraccion -> Int -> Int
-maximaDuracionAtraccion atraccion cantTornillo = min (10) (cantTornillo * duracionAtraccion atraccion)
+maximaDuracionAtraccion atraccion cantTornillo = min (10) (cantTornillo + (duracionAtraccion atraccion))
+
+setearDuracion :: Int -> Atraccion -> Atraccion
+setearDuracion nuevaDuracion atraccion = modificadorDuracionAtraccion (\_ -> nuevaDuracion) atraccion
 
 engrase :: Int -> Trabajo
-engrase grasa atraccion = (modificadorOpiniones (++ ["para valientes"]) . modificadorAlturaMinima ((+) (div (alturaMinima atraccion) 10))) atraccion
+engrase cantGrasa atraccion = (modificadorOpiniones (++ ["para valientes"]) . modificadorAlturaMinima ((+) (div (cantGrasa) 10))) atraccion
+
+-- Aumenta en 0,1 por cada cant. de grasa -> grasaAtraccion + (cantGrasa * 0,1)
 
 -- multiplicar por 0.1  = dividir entre 10
 -- 0.1 * = div 10
@@ -96,10 +105,12 @@ mantenimientoBasico = (engrase 10) . (ajusteDeTornilleria 8)
 -- * Punto 3
 
 meDaMiedito :: Atraccion -> Bool
-meDaMiedito atraccion = ((any (mantenimientoLargo 4)) . ordenReparacion) atraccion
+meDaMiedito atraccion = ((any (mantenimientoMasLargoQueN 4)) . ordenReparacion) atraccion
 
-mantenimientoLargo :: Int -> Reparacion -> Bool
-mantenimientoLargo dias = (>) dias . diasParaReparar
+mantenimientoMasLargoQueN :: Int -> Reparacion -> Bool
+mantenimientoMasLargoQueN dias reparacion = ((<) dias . diasParaReparar) reparacion
+
+-- dias < diasParaReparar
 
 -- Supongo que con "cerrar" se refiere a poner el atributo enMantenimiento = True
 -- y no devolver un falso desde la funcion cerramos
@@ -109,10 +120,16 @@ cerramos atraccion
   | otherwise = atraccion
 
 muchaReparacion :: [Reparacion] -> Bool
-muchaReparacion listaReparaciones = (((>) 7) . sum . map diasParaReparar) listaReparaciones
+muchaReparacion listaReparaciones = (((==) 7) . sum . map diasParaReparar) listaReparaciones
+
+cerramos_2 :: Atraccion -> Bool
+cerramos_2 atraccion = (muchaReparacion . ordenReparacion) atraccion
 
 disneyNoEsistis :: Parque -> Bool
-disneyNoEsistis parque = all (\atraccion -> (null . ordenReparacion) atraccion) ((filter (\atraccion -> ((> 5) . length . nombre) atraccion)) parque)
+disneyNoEsistis parque = ((all (null . ordenReparacion)) . filter ((<) 5 . length . nombre)) parque
+
+-- ! MAL disneyNoEsistis parque = all (\atraccion -> (null . ordenReparacion) atraccion) ((filter (\atraccion -> ((> 5) . length . nombre) atraccion)) parque)
+-- ! MAL  disneyNoEsistis parque = all ((null . ordenReparacion) ) ((filter ((> 5) . length . nombre)) parque)
 
 -- Con pura composición, ilegible
 -- disneyNoEsistis parque = ((all (\atraccion -> (null . ordenReparacion) atraccion)) . (filter (\atraccion -> ((> 5) . length . nombre) atraccion))) parque
@@ -128,14 +145,16 @@ disneyNoEsistis parque = all (\atraccion -> (null . ordenReparacion) atraccion) 
 aplicarUnaReparacion :: Reparacion -> Atraccion -> Atraccion
 aplicarUnaReparacion reparacion atraccion = (efectosDeReperacion . (trabajoReparacion reparacion)) atraccion
 
+-- (trabajoReparacion reparacion) me trae el trabajo (una funcion) que es aplicada a atraccion
+
 reparacionPeola :: Atraccion -> Bool
-reparacionPeola atraccion = (sonPeolas atraccion . ordenReparacion) atraccion
+reparacionPeola atraccion = ((sonPeolas atraccion) . ordenReparacion) atraccion
 
 sonPeolas :: Atraccion -> [Reparacion] -> Bool
 sonPeolas _ [] = True
-sonPeolas _ [trabajo] = True
+-- sonPeolas _ [trabajo] = True -- !No es necesario porque agarra la cola vacia y te da true
 sonPeolas atraccion (primerReparacion : reparaciones) =
-  (queTanBuenaEs . (aplicarUnaReparacion primerReparacion)) atraccion < queTanBuenaEs atraccion
+  (queTanBuenaEs . (aplicarUnaReparacion primerReparacion)) atraccion > queTanBuenaEs atraccion
     && sonPeolas (aplicarUnaReparacion primerReparacion atraccion) reparaciones
 
 -- !Hay que tener cuidado en mantener el efecto de la cabeza, para que se "propage" el efecto.
@@ -147,15 +166,40 @@ realizarProcesoDeReparacion :: Atraccion -> Atraccion
 realizarProcesoDeReparacion atraccion =
   foldr aplicarUnaReparacion atraccion (ordenReparacion atraccion)
 
--- La funcion dentro del foldr debe recibir un parametro de del tipo de la lista y de la semilla
+-- foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
+
+-- La funcion dentro del foldr debe recibir un parametro del tipo de la lista y de la semilla
 --   Rep1 (lambda) (Rep2 (lambda) (Rep3 (lambda) semilla))
 
 {-
-   Ejemplo de evaluación por consola[cite: 40]:
+   Ejemplo de evaluación por consola:
 
    Supongamos una montaña rusa con 2 reparaciones:
    montaniaRusa :: Atraccion
    montaniaRusa = Atraccion "Montaña Rusa" 140 5 ["embole"] True [Reparacion 2 (ajusteDeTornilleria 5), Reparacion 1 mantenimientoElectrico]
    realizarProcesoDeReparacion (montaniaRusa)
+-}
 
+-- * Punto 6
+
+{-
+? Punto 5: Manny a la obra (El proceso de trabajo)
+
+No es posible obtener un valor computable.
+¿Por qué? Para que Haskell te pueda mostrar el resultado de la Atracción (su
+nueva duración, altura o nombre), primero tiene que terminar de aplicar todas
+las funciones de la lista.
+Si la lista de trabajos es infinita, el programa se queda "trabajando" para siempre
+y nunca llega a escupir el resultado final por consola.
+
+? Punto 4: Reparaciones peolas (La comparación)
+Sí es posible obtener un resultado en un caso específico.
+El "atajo": Como esta función devuelve un Bool usando el operador &&, Haskell usa
+algo llamado cortocircuito.
+
+La lógica: Si la función encuentra una sola reparación que no sea "peola" (que no
+mejore el puntaje), el resultado automáticamente es False.
+Gracias a la Evaluación Diferida, Haskell deja de mirar el resto de la lista infinita
+en cuanto encuentra ese primer False y te da la respuesta al toque.
+Si todas fueran "peolas", ahí sí se colgaría buscando el final.
 -}
