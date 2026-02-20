6ef3a62c6f6ed754e0ad88921b9d4c45915dcdc6
Salvatierra-Benjamin <201190726+Salvatierra-Benjamin@users.noreply.github.com>
2026-02-20 17:52:16 -0300

    aventureros actualizado

--- Haskell/jueves-noche/Practica-Aventureros/aventureros.hs
+++ Haskell/jueves-noche/Practica-Aventureros/aventureros.hs
@@ -1,16 +1,16 @@
-data Aventurero = Aventurero
-  { nombre :: String,
-    carga :: Int,
-    salud :: Int,
-    coraje :: Bool,
-    criterio :: CriterioEleccion
-    -- el criterio Recibe un aventurero ya modificado, y con ese resultado
-    -- de la transformacion verifica si esta conforme o no.
-  }
+-- data Aventurero = Aventurero
+--   { nombre :: String,
+--     carga :: Int,
+--     salud :: Int,
+--     coraje :: Bool,
+--     criterio :: CriterioEleccion
+--     -- el criterio Recibe un aventurero ya modificado, y con ese resultado
+--     -- de la transformacion verifica si esta conforme o no.
+--   }
 
-type CriterioEleccion = Aventurero -> Bool
+-- type CriterioEleccion = Aventurero -> Bool
 
-type Encuentro = Aventurero -> Aventurero
+-- type Encuentro = Aventurero -> Aventurero
 
 conformista :: CriterioEleccion
 conformista _ = True
@@ -25,18 +25,18 @@ valiente aventurero = ((<) 50 . salud) aventurero || coraje aventurero
 -- tieneCorajeOSaludMayor50 aventurero = ((<) 50 . salud) aventurero || coraje aventurero
 
 lightPacker :: Int -> CriterioEleccion
-lightPacker umbral = (>) umbral . carga
+lightPacker umbral aventurero = ((>) umbral . carga) aventurero
 
 -- * Punto 2
 
 algunoTieneNombreLargo :: [Aventurero] -> Bool
-algunoTieneNombreLargo = any tieneNombreLargo
+algunoTieneNombreLargo aventureros = any tieneNombreLargo aventureros
 
 tieneNombreLargo :: Aventurero -> Bool
-tieneNombreLargo = (<) 5 . length . nombre
+tieneNombreLargo aventurero = ((<) 5 . length . nombre) aventurero
 
 cargaTotalDeNumerosPares :: [Aventurero] -> Int
-cargaTotalDeNumerosPares = sum . map carga . filter (tieneCargaPar)
+cargaTotalDeNumerosPares aventureros = (sum . map carga . filter (tieneCargaPar)) aventureros
 
 tieneCargaPar :: Aventurero -> Bool
 tieneCargaPar = even . carga
@@ -68,6 +68,10 @@ setearCriterio criterioNuevo aventurero = aventurero {criterio = criterioNuevo}
 curandero :: Aventurero -> Aventurero
 curandero aventurero = (ofrecerSouvenir . modificadorSalud ((+) (div (salud aventurero) 5)) . modificadorCarga (flip div 2)) aventurero
 
+-- el 20% de algo es igual a dividir por 5
+-- 20% = 20/100 -> 1/5
+-- Lo mismo que dividir entre 5
+
 inspirador :: Aventurero -> Aventurero
 inspirador aventurero = (ofrecerSouvenir . modificadorSalud ((+) (div (salud aventurero) 10)) . modificadorCoraje True) aventurero
 
@@ -76,12 +80,39 @@ embaucador = (ofrecerSouvenir . setearCriterio (lightPacker 10) . modificadorSal
 
 -- * Punto 4
 
+data Aventurero = Aventurero
+  { nombre :: String,
+    carga :: Int,
+    salud :: Int,
+    coraje :: Bool,
+    criterio :: CriterioEleccion
+    -- el criterio Recibe un aventurero ya modificado, y con ese resultado
+    -- de la transformacion verifica si esta conforme o no.
+  }
+
+type CriterioEleccion = Aventurero -> Bool
+
+type Encuentro = Aventurero -> Aventurero
+
 encuentrosEnfrentables :: Aventurero -> [Encuentro] -> [Encuentro]
 encuentrosEnfrentables _ [] = []
 encuentrosEnfrentables aventurero (primerEncuentro : resto)
-  -- (criterio aventurero) me trae el criterio (en el enunciado como ej. Valiente) y es
-  -- aplicado al aventurero modificado por primerEncuentro
-  -- \| (criterio aventurero) (primerEncuentro aventurero) =
-  | (criterio (primerEncuentro aventurero) . primerEncuentro) aventurero =
+  -- (criterio aventurero) me devuelve el criterio, y se le pasa el
+  -- aventurero ya modificado por el primerEncuentro
+  -- Si se le setea el criterio, se evalua con el criterio viejo
+  -- como quedo el aventurero ya modificado
+  | ((criterio aventurero) . primerEncuentro) aventurero =
       primerEncuentro : encuentrosEnfrentables (primerEncuentro aventurero) resto
   | otherwise = []
+
+-- (criterio aventurero) me trae el criterio (en el enunciado como ej. Valiente) y es
+-- aplicado al aventurero modificado por primerEncuentro
+-- \| (criterio aventurero) (primerEncuentro aventurero) =
+
+-- * Punto 5
+
+{-
+Se puede determinar el estado final solo si deja de cumplir el criterio del seleccion.
+De lo contrario, si fuera conformista, no se podria determinar porque el aventurero
+no pararia de aceptar encuentros ergo su estado siempre cambiara.
+-}
