4283330d743703343731735e3e01721184f56496
Salvatierra-Benjamin <201190726+Salvatierra-Benjamin@users.noreply.github.com>
2026-02-20 21:53:10 -0300

    musicos actualizado

--- Haskell/jueves-noche/Practica-Musicos/musicos.hs
+++ Haskell/jueves-noche/Practica-Musicos/musicos.hs
@@ -16,7 +16,7 @@ data Actuacion = Actuacion
 -- * Punto 1
 
 actuacionMayor500 :: Musico -> Bool
-actuacionMayor500 = any (> 500) . map publico . historialActuacion
+actuacionMayor500 musico = (any (> 500) . map publico . historialActuacion) musico
 
 -- :t snd
 -- snd :: (a, b ) -> b
@@ -24,7 +24,16 @@ actuacionMayor500 = any (> 500) . map publico . historialActuacion
 -- tercerElementoTupla :: (a , b , c) -> c
 -- tercerElementoTupla (_ , _ , x) = x
 publicoPorAnio :: Int -> Musico -> Int
-publicoPorAnio anio musico = (publico . head . filter ((==) anio . (\(_, _, a) -> a) . fecha) . historialActuacion) musico
+publicoPorAnio anio musico = (length . filter ((==) anio . (\(_, _, a) -> a) . fecha) . historialActuacion) musico
+
+-- !No se permitia funciones auxiliares
+-- publicoPorAnio anio musico = (length . filter (coincideFechaConActuacion anio) . historialActuacion) musico
+
+-- anioGetter :: (a, b, c) -> c
+-- anioGetter (_, _, c) = c
+
+-- coincideFechaConActuacion :: Int -> Actuacion -> Bool
+-- coincideFechaConActuacion anioBuscado actuacion = ((==) anioBuscado . anioGetter . fecha) actuacion
 
 -- * Punto 2
 
@@ -33,6 +42,8 @@ type Actividad = Musico -> Musico
 tocarInstrumento :: String -> Actividad
 tocarInstrumento nuevoInstrumento musico = (sumarExperienciaPorInstrumento . setearInstrumento nuevoInstrumento) musico
 
+-- Es una forma diferente. Seteo el nuevo instrumento como favorito (el que vino por parametro)
+-- Si el nuevo instrumento favorito es "oboe faggot cello" entonce sumo 1 de Exp
 setearInstrumento :: String -> Musico -> Musico
 setearInstrumento nuevoInstrumento musico = musico {instrumentoFavorito = nuevoInstrumento}
 
@@ -42,7 +53,10 @@ sumarExperienciaPorInstrumento musico
   | otherwise = musico
 
 sumar1DeExperiencia :: Musico -> Musico
-sumar1DeExperiencia musico = musico {gradoExp = ((+ 1) . gradoExp) musico}
+sumar1DeExperiencia musico = musico {gradoExp = min 10 (gradoExp musico + 1)}
+
+-- modificadorExperiencia :: (Int -> Int) -> Musico -> Musico
+-- modificadorExperiencia modificador musico = musico { gradoExp = (modificador . gradoExp) musico}
 
 tocaOboeFagotoOCello :: String -> Bool
 tocaOboeFagotoOCello "oboe" = True
@@ -54,7 +68,17 @@ modificarInstrumentoFavorito :: (String -> String) -> Musico -> Musico
 modificarInstrumentoFavorito modificador musico = musico {instrumentoFavorito = (modificador . instrumentoFavorito) musico}
 
 cantar :: Actividad
-cantar = modificarInstrumentoFavorito (++ "Lalala")
+cantar = modificarInstrumentoFavorito ((++) "Lalala")
+
+presentarPro :: Actuacion -> Actividad
+presentarPro actuacion musico = (sumarExperienciaPorInstrumento . sumar1DeExperiencia . modificarHistorial (flip (++) [actuacion])) musico
+
+-- (++) nuevaCabeza cola
+-- flip (++) cola  nuevaCabeza
+-- ghci> (++) "hola" "chau"
+-- "holachau"
+-- ghci> flip (++) "hola" "chau"
+-- "chauhola"
 
 presentar :: Int -> (Int, Int, Int) -> Actividad
 presentar publico fecha musico = (sumarExperienciaPorInstrumento . agregarPresentacion publico fecha) musico
@@ -77,13 +101,21 @@ pepe = Musico "pepe" 5 "piano" []
 prueba :: Musico -> Musico
 prueba musico = (pensar . presentar 300 (2, 8, 2025) . cantar . tocarInstrumento "faggot") musico
 
+prueba2 :: Musico -> Musico
+prueba2 musico = (pensar . presentarPro (Actuacion (2, 8, 2025) 300) . cantar . tocarInstrumento "faggot") musico
+
 -- * Punto 3
 
 cuantaExperiencia :: Musico -> [Actividad] -> Int
 cuantaExperiencia musico actividades = (gradoExp . hacerActividades actividades) musico
 
 hacerActividades :: [Actividad] -> Musico -> Musico
-hacerActividades actividades musico = foldr ($) musico actividades
+-- hacerActividades actividades musico = foldr ($) musico actividades
+hacerActividades actividades musico = foldl (\musico actividad -> actividad musico) musico actividades
+
+-- foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+
+-- * Punto 4
 
 -- No es necesario verificar que la posicion UNO es una posicion Par...
 -- La paridad de las posiciones siempre es la misma y es conocida.
@@ -94,6 +126,6 @@ presentacionesCorrectas musico = (correspondeParidad . map publico . historialAc
 
 correspondeParidad :: [Int] -> Bool
 correspondeParidad [] = True
-correspondeParidad [soloUnNumero] = True
+correspondeParidad [soloUnNumero] = odd soloUnNumero -- si solo hay un numero en la lista, debe ser impar
 correspondeParidad (primerNumero : segundoNumero : numeros) =
   odd primerNumero && even segundoNumero && correspondeParidad (numeros)
